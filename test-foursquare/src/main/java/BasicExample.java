f1e84cdb213136ae90f1b718572a6d08c4f50bda
aymen <aymen@192.168.1.6>
2015-10-11 14:54:29 +0100

    Signed-off-by: aymen <aymen@192.168.1.6>

---
+++ test-foursquare/src/main/java/BasicExample.java
@@ -0,0 +1,58 @@
+
+
+import fi.foyt.foursquare.api.FoursquareApi;
+import fi.foyt.foursquare.api.FoursquareApiException;
+import fi.foyt.foursquare.api.Result;
+import fi.foyt.foursquare.api.entities.Category;
+import fi.foyt.foursquare.api.entities.CompactVenue;
+import fi.foyt.foursquare.api.entities.VenuesSearchResult;
+
+/**
+ * Basic search example
+ * @TODO - more examples please :)
+ * @author rmangi
+ *
+ */
+public class BasicExample {
+
+	  public static void main(String[] args) {
+	    String ll = args.length > 0 ? args[0] : "44.3,37.2";
+	    try {
+	      (new BasicExample()).searchVenues(ll);
+	    } catch (FoursquareApiException e) {
+	      // TODO: Error handling
+	    }
+	  }
+
+	  public void searchVenues(String ll) throws FoursquareApiException {
+	    // First we need a initialize FoursquareApi. 
+	    FoursquareApi foursquareApi = new FoursquareApi("DWHLE1OQJBM2HENXFNAR1CGPOEYTCEKHRSF5AV5ZXGD5ZT1Q", "P5AKFTGQUJQSYOZMI02KO4H55ZBP1JGQ3CELF44YR2S3XF4D", "http:\\\\localhost\\");
+	    ll= "36.857453, 10.208315";
+	    
+	    // After client has been initialized we can make queries.
+	    Result<VenuesSearchResult> result = foursquareApi.venuesSearch(ll, null, null, null, null, 100, null, "4bf58dd8d48988d1e0931735", null, null, null, null, null);
+	    
+	    //result = foursquareApi.venuesSearch("tunis", "", 100, null, "4bf58dd8d48988d1e0931735", null, null, null);
+	    
+	    //foursquareApi.venuesCategories();
+	    
+	    if (result.getMeta().getCode() == 200) {
+	      // if query was ok we can finally we do something with the data
+	      for (CompactVenue venue : result.getResult().getVenues()) {
+	        // TODO: Do something we the data
+	        System.out.print(venue.getName()+" ("+venue.getCategories().length+")");
+	        Category[] categories= venue.getCategories();
+	        for(int i =0 ; i< categories.length;i++){
+	        	System.out.print(categories[i]+"\t");
+	        }
+	        System.out.println();
+	      }
+	    } else {
+	      // TODO: Proper error handling
+	      System.out.println("Error occured: ");
+	      System.out.println("  code: " + result.getMeta().getCode());
+	      System.out.println("  type: " + result.getMeta().getErrorType());
+	      System.out.println("  detail: " + result.getMeta().getErrorDetail()); 
+	    }
+	  }
+}
