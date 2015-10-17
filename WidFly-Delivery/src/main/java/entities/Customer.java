6b7361e074e46cffcae880038a9f0c9eaa76861e
aymen <aymen@192.168.1.6>
2015-10-17 22:43:47 +0100

    Signed-off-by: aymen <aymen@192.168.1.6>

---
+++ WidFly-Delivery/src/main/java/entities/Customer.java
@@ -0,0 +1,61 @@
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer implements Serializable {
+	
+	private Integer id ;
+	private String full_name ; 
+private static final long serialVersionUID = 1L;
+	
+	public Customer() {
+		super();
+		
+	}
+	
+	
+
+	public Customer(String full_name) {
+		super();
+		this.full_name = full_name;
+	}
+
+
+
+	@Id  
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	public String getFull_name() {
+		return full_name;
+	}
+
+
+	public void setFull_name(String fullname) {
+		this.full_name = fullname;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Customer [fullname=" + full_name + "]";
+	}
+
+
+	
+
+}
---
+++ WidFly-Delivery/src/main/java/entities/DeliveryZone.java
@@ -0,0 +1,38 @@
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class DeliveryZone implements Serializable {
+	
+	private Integer id ;
+private static final long serialVersionUID = 1L;
+	
+	public DeliveryZone() {
+		super();
+		
+	}
+	
+
+	@Id  
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "Delivery Zone [id=" + id + "]";
+	}
+
+
+}
