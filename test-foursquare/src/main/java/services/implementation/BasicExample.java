064871b3cf9de93a81ab68a46e433760c6df886b
DATABIZ <DATABIZ@DATABIZ-PC>
2015-10-19 15:05:02 +0100

    Signed-off-by: DATABIZ <DATABIZ@DATABIZ-PC>

--- test-foursquare/src/main/java/services/implementation/BasicExample.java
+++ test-foursquare/src/main/java/services/implementation/BasicExample.java
@@ -3,7 +3,7 @@ package services.implementation;
 import javax.ejb.Stateless;
 
 import services.interfaces.BasicExampleLocal;
-import fi.foyt.foursquare.api.FoursquareApi;
+import utility.ForsquareUtility;
 import fi.foyt.foursquare.api.FoursquareApiException;
 import fi.foyt.foursquare.api.Result;
 import fi.foyt.foursquare.api.entities.Category;
@@ -17,17 +17,14 @@ public class BasicExample<T> implements BasicExampleLocal<T> {
 	public Result<T> searchRestaurant(String location)
 			throws FoursquareApiException {
 		// First we need a initialize FoursquareApi.
-		FoursquareApi foursquareApi = new FoursquareApi(
-				"DWHLE1OQJBM2HENXFNAR1CGPOEYTCEKHRSF5AV5ZXGD5ZT1Q",
-				"P5AKFTGQUJQSYOZMI02KO4H55ZBP1JGQ3CELF44YR2S3XF4D",
-				"http:\\\\localhost\\");
-		
+
 		location = "36.857453, 10.208315";
 
 		// After client has been initialized we can make queries.
-		Result<VenuesSearchResult> result = foursquareApi.venuesSearch(
-				location, null, null, null, null, 100, null,
-				"4bf58dd8d48988d1e0931735", null, null, null, null, null);
+		Result<VenuesSearchResult> result = ForsquareUtility.getFoursquareApi()
+				.venuesSearch(location, null, null, null, null, 100, null,
+						"4bf58dd8d48988d1e0931735", null, null, null, null,
+						null);
 
 		// result = foursquareApi.venuesSearch("tunis", "", 100, null,
 		// "4bf58dd8d48988d1e0931735", null, null, null);
