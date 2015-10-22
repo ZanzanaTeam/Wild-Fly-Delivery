b2853cad0e50c3ca5d22aa4211df94ca67a114e9
ghoulaymen <ghoolaymen@gmail.com>
2015-10-23 00:36:29 +0100

    Signed-off-by: ghoulaymen <ghoolaymen@gmail.com>

--- WidFly-Delivery/src/main/java/services/implementation/MenuService.java
+++ WidFly-Delivery/src/main/java/services/implementation/MenuService.java
@@ -1,8 +1,13 @@
 package services.implementation;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 
 import services.interfaces.MenuServiceRemote;
+import entities.Menu;
+import entities.Order;
+import entities.Restaurant;
 
 /**
  * Session Bean implementation class MenuService
@@ -22,4 +27,23 @@ public class MenuService implements MenuServiceRemote {
 
 	}
 
+	@Override
+	public void addMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Menu> finMenuByRestaurant(Restaurant restaurant) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Menu> finMenuByOrder(Order order) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
--- WidFly-Delivery/src/main/java/services/interfaces/MenuServiceRemote.java
+++ WidFly-Delivery/src/main/java/services/interfaces/MenuServiceRemote.java
@@ -1,8 +1,21 @@
 package services.interfaces;
 
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import entities.Menu;
+import entities.Order;
+import entities.Restaurant;
 
 @Remote
 public interface MenuServiceRemote {
-	void assignMenu(int menuId , int restaurantId);
+	void assignMenu(int menuId, int restaurantId);
+
+	void addMenu(Menu menu);
+
+	List<Menu> finMenuByRestaurant(Restaurant restaurant);
+
+	List<Menu> finMenuByOrder(Order order);
+
 }
